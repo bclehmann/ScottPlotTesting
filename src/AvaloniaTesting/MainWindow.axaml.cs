0a034b4916db345e0a66dbeab47c2f773627987e
Benjamin Lehmann <bclehmann@yahoo.ca>
2020-07-29 15:25:46 -0600

    Comprehensible folder structure

---
+++ src/AvaloniaTesting/MainWindow.axaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using ScottPlot.Avalonia;
+
+namespace AvaloniaTesting
+{
+	public class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+#if DEBUG
+			this.AttachDevTools();
+#endif
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+
+			this.Find<AvaPlot>("avaplot").plt.PlotScatter(new double[] { 1, 2, 3 }, new double[] { 1, 2, 1 });
+			this.Find<AvaPlot>("avaplot").Render();
+		}
+	}
+}
